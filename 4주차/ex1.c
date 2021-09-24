69f20701c2c891d7c0feaafa1b427c2e25544ab5
narae-yoon <nare6557@gmail.com>
2021-09-24 17:46:21 +0900

    4주차

---
+++ 4주차/ex1.c
@@ -0,0 +1,23 @@
+﻿// 배열 값 중 0을 찾아 배열 뒤로 배치시키기
+
+#include <stdio.h>
+int main(void) {
+  int arr1[] = { 2, 3, 0, 0, 4, 0, 6 };
+  int n = sizeof(arr1) / sizeof(arr1[0]);
+  int j = 0;
+  
+  for (int i = 0; i < n; i++) {
+    if (arr1[i] != 0) {
+      arr1[j] = arr1[i];
+      j = j + 1;
+    }
+  }
+
+  for (int i = j; i < n; i++)
+    arr1[i] = 0;
+
+  for (int i = 0; i < n; i++)
+    printf("%d ", arr1[i]);
+  
+  return 0;
+}
---
+++ 4주차/ex1.py
@@ -0,0 +1,17 @@
+﻿# 배열 값 중 0을 찾아 배열 뒤로 배치시키기
+
+def reorder(arr, n):
+  j = 0 
+  
+  for i in range(n):
+    if arr[i] != 0:
+      arr[j] = arr[i]
+      j+=1
+
+  for i in range(j, n):
+    arr[i] = 0
+
+arr = [6, 0, 8, 2, 3, 0, 4, 0, 1]
+n = len(arr)
+reorder(arr, n)
+print(arr)
