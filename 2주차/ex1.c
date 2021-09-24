ba9373fac489fa8e32cd2493f8c6d2bf286eec05
narae-yoon <nare6557@gmail.com>
2021-09-24 17:52:24 +0900

    2주차 추가

---
+++ 2주차/ex1.c
@@ -0,0 +1,18 @@
+﻿// 거스름돈 문제
+
+#include <stdio.h>
+int main (void) {
+  int n = 1260;
+  int cnt = 0;
+  int coinType[4] = {500, 100, 50, 10};
+  int size = sizeof(coinType) / sizeof(coinType[0]);
+
+  for (int i = 0; i < size; i++) {
+    cnt += n / coinType[i];
+    n %= coinType[i];
+  }
+  
+  printf("count = %d" , cnt);
+
+  return 0;
+}
---
+++ 2주차/ex1.py
@@ -0,0 +1,12 @@
+﻿# 거스름돈 문제
+
+n = 1260
+cnt = 0
+
+array = [500, 100, 50, 10]
+
+for coin in array:
+  cnt += n // coin
+  n %= coin
+  
+print(cnt)
