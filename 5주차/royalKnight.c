a00048b2a878beff4cebffb44cd3e30ff74e67c2
narae-yoon <nare6557@gmail.com>
2021-10-05 06:13:57 +0900

    5주차

---
+++ 5주차/royalKnight.c
@@ -0,0 +1,27 @@
+﻿// 왕실의 나이트
+
+#include <bits/stdc++.h>
+
+using namespace std;
+string inputData;
+int dx[] = {-2, -1, 1, 2, 2, 1, -1, -2};
+int dy[] = {-1, -2, -2, -1, 1, 2, 2, 1};
+
+int main(void) {
+  cin >> inputData;
+  int row = inputData[1] - '0';
+  int column = inputData[0] - 'a' + 1;
+  int result = 0;
+  
+  for (int i = 0; i < 8; i++) {
+    int nextRow = row + dx[i];
+    int nextColumn = column + dy[i];
+    
+    if (nextRow >= 1 && nextRow <= 8 && nextColumn >= 1 && nextColumn <= 8) {
+      result += 1;
+    }
+  }
+  
+  cout << result << '\n';
+  return 0;
+}
---
+++ 5주차/upDown.c
@@ -0,0 +1,39 @@
+﻿// 상하좌우
+
+#include <bits/stdc++.h>
+
+using namespace std;
+int n;
+string plans;
+int x = 1, y = 1;
+
+int dx[4] = {0, 0, -1, 1};
+int dy[4] = {-1, 1, 0, 0};
+char moveTypes[4] = {'L', 'R', 'U', 'D'};
+
+int main(void) {
+  cin >> n;
+  cin.ignore();
+  getline(cin, plans);
+
+  for (int i = 0; i < plans.size(); i++) {
+    char plan = plans[i];
+    int nx = -1, ny = -1;
+
+    for (int j = 0; j < 4; j++) {
+      if (plan == moveTypes[j]) {
+        nx = x + dx[j];
+        ny = y + dy[j];
+      }
+    }
+
+    if (nx < 1 || ny < 1 || nx > n || ny > n)
+      continue;
+
+    x = nx;
+    y = ny;
+  }
+  
+  cout << x << ' ' << y << '\n';
+  return 0;
+}
