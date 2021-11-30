a2c00a24162afef34bf09f32fd0ec10a53ad26e8
narae-yoon <nare6557@gmail.com>
2021-11-30 20:30:31 +0900

    13주차

---
+++ 13주차/secretMap.py
@@ -0,0 +1,14 @@
+﻿arr1 = [9, 20, 28, 18, 11]
+arr2 = [30, 1, 21, 17, 28]
+n = len(arr1)
+
+for i in range(n):
+  row = arr1[i] | arr2[i]
+
+  print("[", end="")
+  for j in range(n-1, -1, -1):
+    if ((row >> j) & 1) == 1:
+      print("#", end="")
+    else:
+      print(" ", end="")
+  print("]")
---
+++ 13주차/stringComp.py
@@ -0,0 +1,34 @@
+﻿def solution(s):
+  answer = len(s)
+
+  for i in range(1, int(len(s)/2)+1):
+    length = len(s)
+
+    for j in range(len(s)):
+      z = i
+      cnt = 0
+
+      while j + z < len(s):
+        if s[j:j+i] == s[j+z:j+z+i]:
+          cnt += 1
+        else:
+          length -= i * cnt
+          if cnt:
+            length += len(str(cnt+1))
+          j += z-1
+          break
+        
+        if j+z+i >= len(s):
+          length -= i * cnt
+          length += len(str(cnt+1))
+          j += z
+
+        z += i 
+
+    if length < answer:
+      answer = length
+
+  return answer
+
+s = "abcabcdede"
+print(solution(s))
